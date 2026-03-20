a0d1dd2eaec45388dad4858ca740965e1cb26162
RodrigoZbrgs <rodrigo.zandbergs@gmail.com>
2026-03-20 18:33:40 +0200

    Added getters and setter for MyNode class

--- src/datastr/MyNode.java
+++ src/datastr/MyNode.java
@@ -1,5 +1,53 @@
 package datastr;
 
 public class MyNode {
+	private char element;
+	
+	private MyNode nextNode = null;
+	
+	private MyNode previousNode = null;
 
+	public char getElement() {
+		return element;
+	}
+
+	public MyNode getPreviousNode() {
+		return previousNode;
+	}
+
+	public MyNode getNextNode() {
+		return nextNode;
+	}
+	
+	public void setElement(char element) { //kad pariesim uz ttype, tad japarbauda.
+		this.element = element;
+	}
+	
+	public void setNextNode(MyNode nextNode) {
+		this.nextNode = nextNode;
+	}
+	
+	public void setPreviousNode(MyNode previousNode) {
+		this.previousNode = previousNode;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
